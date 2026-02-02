866f2d8ef8d26cb29994a9d15d2a7c12a58c9713
zarnab123713 <eyesangel787@gmail.com>
2026-02-02 10:13:42 -0800

    Add files via upload

---
+++ 6.Operator.js
@@ -0,0 +1,45 @@
+//Arithemati operetor
+let a= 5;
+let b= 2;
+console.log("a"=,a,"& b=",b);
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a * b =",a*b);
+console.log("a / b =",a/b);
+console.log("a ** b =",a**b);
+console.log("a % b =",a%b);
+
+//unary operator
+let a = 5;
+let b = 2;
+console.log("a++ =",a++);
+console.log("a=",a);
+console.log("a-- =",a--);
+console.log("a=",a);
+console.log("++b =",++b);
+console.log("b=",b);
+console.log("--b =",b);
+console.log("b=",b);
+
+//logical operator
+let a=6;
+let b=5;
+cond1=a<b;
+cond2=a>b;
+console.log("cond1 || cond2 =",cond1||cond2);//OR operator
+console.log("cond1 && cond2 =",cond1&&cond2);//AND operator
+console.log("!(cond1)",!(cond1));//NOT Operator
+
+//comparison operator
+let a=6;
+let b=5;
+console.log("6>=5",a>=b);
+console.log("a==b",a==b);
+
+//assignment operator
+let a=5;
+let b=2;
+a**=4;//a=a**4
+console.log("a=",a);
+b+=4;//b=b+4
+console.log("b=",b);
