6ce6ae2b442d5f0c7484517512086f2312144ad1
zarnab123713 <eyesangel787@gmail.com>
2026-02-02 10:15:22 -0800

    Add files via upload

---
+++ 11.Arrays.js
@@ -0,0 +1,11 @@
+//code1 using for-of loop
+let cities=["lahore","karachi","multan"];
+for(let city of cities){
+    console.log(city);
+}
+
+//code2 using for loop
+let heroes=["thor","hulk","spiderman","ironman"];
+for(let idx=0; idx<heroes.length; idx++){
+    console.log(heroes[idx]);
+}
