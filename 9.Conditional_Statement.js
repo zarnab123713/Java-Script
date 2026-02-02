f875eca5ca7604be6e34ae209ff61291cb2caf69
zarnab123713 <eyesangel787@gmail.com>
2026-02-02 10:14:41 -0800

    Add files via upload

---
+++ 9.Conditional_Statement.js
@@ -0,0 +1,43 @@
+//Ternary operator
+let age =25;
+let result=age>=18 ? "adult":"not adult";
+console.log(result);
+
+//if-statement
+let mode="dark";
+let color;
+if(mode==="dark"){
+    color="black";
+}
+if(mode==="light"){
+    color="White";
+}
+console.log(color);
+
+
+//if-else statement
+let mode="blue";
+let color;
+if (mode==="dark"){
+    color="black";
+}
+else{
+    color="white";
+}
+console.log(color);
+
+
+//if-else-if Statement
+let mode="blue";
+let color;
+if (mode==="dark"){
+    color="black";
+}else if(mode==="blue"){
+    color="blue";
+}else if(mode==="pink"){
+    color="pink";
+}else{
+    color="white";
+}
+console.log(color);
+
