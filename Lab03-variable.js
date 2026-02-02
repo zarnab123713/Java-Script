81aac53aeae3ce96482db094c8e632ec80b785bf
Zarnab Fatima <eyesangel787@gmail.com>
2026-02-02 10:28:07 -0800

    Add files via upload

---
+++ Lab03-variable.js
@@ -0,0 +1,22 @@
+//variable decleration
+name="silent eXpert";
+age=24;
+price=123;
+console.log(name);
+console.log(age);
+console.log(price);
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
+isfollow=false;
+console.log(isfollow);
+//let
+let fullname= AudioListener;
+console.log(fullname);
+//var
+var age=26;
+console.log(age);
+//costant
+const PI=3.14;
+console.log(PI);
