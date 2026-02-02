a84ac50a7e841d0bc18522f4021e2f74d145adfe
Zarnab Fatima <eyesangel787@gmail.com>
2026-02-02 10:28:27 -0800

    Add files via upload

---
+++ Lab04-Premitive_DataType.js
@@ -0,0 +1,19 @@
+//Number
+let age = 24;
+console.log(age);
+//Boolean
+isFollow=true;
+console.log(isFollow);
+//String
+name="Ali Ahmed";
+console.log(name);
+//udefined
+let x;
+console.log(x);
+//by default value of x is undefined and datatype is also undefined
+//BigInt
+let x= BigInt("123");
+console.log(x);
+//symbol
+let y= Symbol("Hellow");
+console.log(y);
