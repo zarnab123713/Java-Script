92d71e637f2448515ebe564fd4785246a634608b
Zarnab Fatima <eyesangel787@gmail.com>
2026-02-02 10:32:25 -0800

    Add files via upload

---
+++ Lab08-Template_literals.js
@@ -0,0 +1,11 @@
+let specialString='this is tempelate literal';
+console.log(typeof specialString);
+
+let obj={
+    item="pen",
+    price:10,
+};
+let output='the cost of ${obj.item} is ${obj.price} rupees';
+console.log(output);
+console.log("the cost of",obj.item,"is",obj.price,"rupees");
+
