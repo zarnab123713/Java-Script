42976f0dc1042385db8b422d42e330b18f6548e4
zarnab123713 <eyesangel787@gmail.com>
2026-02-02 10:13:15 -0800

    Add files via upload

---
+++ 5.Non-premitive_DataType.js
@@ -0,0 +1,16 @@
+//Object
+const student={
+    fullname:"Silent eXpert",
+    age:20,
+    cgpa:3.8,
+    isPass=true,
+};
+console.log("full name=",student[fullname]
+);
+const profile={
+    username:"sharadakhapra",
+    isFollow:false,
+    followers:123,
+    following:123,
+};
+console.log(typeof profile["followers"]);
